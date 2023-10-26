b978ee672d9dbabe4135262d7c65e05d71f26966
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-10-26 17:12:07 +0530

    Feat: Added tracker app setup & navigations & navigator

---
+++ src/screens/AppNavigator.js
@@ -0,0 +1,92 @@
+// import React from "react";
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+// import AccountScreen from "./src/screens/AccountScreen";
+// import SigninScreen from "./src/screens/SigninScreen";
+// import SignupScreen from "./src/screens/SignupScreen";
+// import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+// import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+// import TrackListScreen from "./src/screens/TrackListScreen";
+
+// const Stack = createStackNavigator();
+
+// const AppNavigator = () => {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Signin" component={SigninScreen} />
+//       <Stack.Screen name="Signup" component={SignupScreen} />
+//     </Stack.Navigator>
+//   );
+// };
+// const Tab = createMaterialBottomTabNavigator();
+
+// return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
+//       <Tab.Screen name="Account" component={AccountScreen} />
+//     </Tab.Navigator>
+//   );
+
+// export default AppNavigator;
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import AccountScreen from './AccountScreen';
+import TrackCreateScreen from './TrackCreateScreen';
+import TrackListScreen from './TrackListScreen';
+import TrackDetailScreen from './TrackDetailScreen';
+import SignupScreen from './SignupScreen';
+import SigninScreen from './SigninScreen';
+
+
+// Create navigation stacks and tabs
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+
+
+
+// Define your navigation structure
+function LoginFlow() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Signup" component={SignupScreen} />
+      <Stack.Screen name="Signin" component={SigninScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function TrackListFlow() {
+  return (
+    <Stack.Navigator>   
+      <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function MainFlow() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="trackListFlow" component={TrackListScreen} />
+      <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />      
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Tab.Navigator>
+  );
+}
+
+// Finally, create the main navigation container
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="loginFlow" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="loginFlow" component={LoginFlow} />
+        <Stack.Screen name="mainFlow" component={MainFlow} />
+        <Stack.Screen name="tracklistflow" component={TrackListFlow} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+
+const SignupScreen = ({ navigation }) => {
+    return (
+        <View>
+        <Spacer>
+           <Text h3>Sign Up for Tracker</Text>
+           </Spacer>
+           <Input label="Email"/>
+           <Spacer />
+           <Input label="Password"/>
+           <Spacer>
+           <Button title="Sign Up" />
+           </Spacer>
+        </View>
+    );
+};
+
+SignupScreen.navigationOptions = () => {
+    return {
+      headerShown: false,
+    };
+  }; 
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
